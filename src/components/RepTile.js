59f4f4320d195a41d2b8defd99d37dd574b559fc
Alex Doss <alex.doss42@gmail.com>
2019-09-10 13:59:46 -0600

    Adds click on name logic

--- src/components/RepTile.js
+++ src/components/RepTile.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RepTile = props => {
+
+  const [ repDetails, setRepDetails ] = useState(false)
+
+  const showDetailsHandler = () => {
+    setRepDetails(!repDetails)
+    console.log('You should now see all the details of the rep', repDetails)
+  }
+
   return (
     <div>
-      <h3>{props.rep.name}</h3>
+      <h3 onClick={showDetailsHandler}>{props.rep.name}</h3>
       <h3>{props.rep.party}</h3>
     </div>
   )
